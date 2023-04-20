eca8aed588aebb3477b598e1cb8ef4a89b7a0ff1
CharlesAdimah <charlesosejob@gmail.com>
2023-04-20 18:38:51 +0000

    Task 0

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -9,10 +9,10 @@ app = Flask(__name__)
 
 
 @app.route('/', strict_slashes=False)
-def hello_route():
+def hello():
     """Displays 'Hello HBNB!'"""
-    return "Hello HBNB!"
+    return 'Hello HBNB!'
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
